89ece74c45f6ec5d8c1841fff97266a12c3abfbe
Ye Chang <yech1990@gmail.com>
2021-02-20 01:21:10 +0800

    quick update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,283 +17,274 @@ end
 
 local packer = require("packer")
 
-local function packer_start()
-  packer.init(
-    {
-      ensure_dependencies = true,
-      git = {
-        cmd = "git",
-        depth = 1,
-        clone_timeout = 600
-      }
+packer.init(
+  {
+    ensure_dependencies = true,
+    git = {
+      cmd = "git",
+      depth = 1,
+      clone_timeout = 600
     }
-  )
+  }
+)
 
-  packer.startup(
-    function()
-      -- Packer can manage itself as an optional plugin
-      use {"wbthomason/packer.nvim", opt = true}
+return packer.startup(
+  function()
+    -- Packer can manage itself as an optional plugin
+    use {"wbthomason/packer.nvim", opt = true}
 
-      -- Load on an autocommand event
-      use {"andymass/vim-matchup", event = "VimEnter *"}
+    -- Load on an autocommand event
+    use {"andymass/vim-matchup", event = "VimEnter *"}
 
-      -- Buffer Line (top)
-      use {
-        "akinsho/nvim-bufferline.lua",
-        config = function()
-          require("conf.bufferline")
-        end,
-        requires = {"kyazdani42/nvim-web-devicons"}
-      }
+    -- Buffer Line (top)
+    use {
+      "akinsho/nvim-bufferline.lua",
+      config = function()
+        require("conf.bufferline")
+      end,
+      requires = {"kyazdani42/nvim-web-devicons"}
+    }
 
-      -- Status Line (bottom)
-      use {
-        "glepnir/galaxyline.nvim",
-        branch = "main",
-        config = function()
-          require("conf.statusline")
-        end,
-        requires = {"kyazdani42/nvim-web-devicons", opt = true}
-      }
+    -- Status Line (bottom)
+    use {
+      "glepnir/galaxyline.nvim",
+      branch = "main",
+      config = function()
+        require("conf.statusline")
+      end,
+      requires = {"kyazdani42/nvim-web-devicons", opt = true}
+    }
 
-      -- Startup window
-      use {
-        "glepnir/dashboard-nvim",
-        config = function()
-          require("conf.dashboard")
-        end
-      }
+    -- Startup window
+    use {
+      "glepnir/dashboard-nvim",
+      config = function()
+        require("conf.dashboard")
+      end
+    }
 
-      -- Syntax
-      use {
-        "nvim-treesitter/nvim-treesitter",
-        event = "BufRead *",
-        after = "telescope.nvim",
-        config = function()
-          vim.api.nvim_command("set foldmethod=expr")
-          vim.api.nvim_command("set foldexpr=nvim_treesitter#foldexpr()")
-          require "nvim-treesitter.configs".setup {
-            highlight = {
-              enable = true
-            },
-            textobjects = {
-              select = {
-                enable = true,
-                keymaps = {
-                  ["af"] = "@function.outer",
-                  ["if"] = "@function.inner",
-                  ["ac"] = "@class.outer",
-                  ["ic"] = "@class.inner"
-                }
+    -- Syntax
+    use {
+      "nvim-treesitter/nvim-treesitter",
+      event = "BufRead *",
+      after = "telescope.nvim",
+      config = function()
+        vim.api.nvim_command("set foldmethod=expr")
+        vim.api.nvim_command("set foldexpr=nvim_treesitter#foldexpr()")
+        require "nvim-treesitter.configs".setup {
+          highlight = {
+            enable = true
+          },
+          textobjects = {
+            select = {
+              enable = true,
+              keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner"
               }
-            },
-            ensure_installed = "all"
-          }
-        end
-      }
-      use {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        after = "nvim-treesitter"
-      }
-
-      -- lua keymap
-      use "tjdevries/astronauta.nvim"
-
-      -- speed up jk
-      use "rhysd/accelerated-jk"
-
-      -- Indent Guides
-      use {
-        "glepnir/indent-guides.nvim",
-        config = function()
-          require("indent_guides").setup(
-            {
-              indent_levels = 30,
-              indent_guide_size = 1,
-              indent_start_level = 1,
-              indent_space_guides = true,
-              indent_tab_guides = false,
-              even_colors = {fg = "#ABB0BA", bg = "#34383F"},
-              odd_colors = {fg = "#BBC2CF", bg = "#2E323A"}
             }
-          )
-        end
-      }
-
-      -- Commentary
-      use "tomtom/tcomment_vim"
-
-      -- Color highlighter
-      use {
-        "norcalli/nvim-colorizer.lua",
-        config = function()
-          require("colorizer").setup()
-        end
-      }
-
-      -- QuickRun
-      use {
-        "thinca/vim-quickrun",
-        setup = function()
-          vim.g.quickrun_no_default_key_mappings = 1
-        end,
-        config = function()
-          vim.api.nvim_set_keymap("n", "<leader>r", "<plug>QuickRun -mode n<cr>", {silent = true, noremap = true})
-          vim.api.nvim_set_keymap("v", "<leader>r", "<plug>QuickRun -mode v<cr>", {silent = true, noremap = true})
-        end
-      }
-
-      -- Fuzzy finder
-      -- use {"liuchengxu/vim-clap"}
-
-      use {
-        "nvim-telescope/telescope.nvim",
-        requires = {{"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"}},
-        config = function()
-          require("conf.telescope")
-        end
-      }
-
-      use {
-        "nvim-telescope/telescope-frecency.nvim",
-        requires = {
-          "tami5/sql.nvim"
-        },
-        config = function()
-          require "telescope".load_extension("frecency")
-          vim.api.nvim_set_keymap(
-            "n",
-            "<Leader>fm",
-            "<Cmd>lua require('telescope').extensions.frecency.frecency()<CR>",
-            {noremap = true, silent = true}
-          )
-        end
-      }
-
-      -- File explorer
-      use {
-        "kyazdani42/nvim-tree.lua",
-        cmd = {"NvimTreeToggle", "NvimTreeOpen"},
-        requires = {"kyazdani42/nvim-web-devicons"},
-        config = function()
-          vim.g.nvim_tree_follow = 1
-          vim.g.nvim_tree_hide_dotfiles = 1
-          vim.g.nvim_tree_indent_markers = 1
-          vim.g.nvim_tree_bindings = {
-            edit = {"<CR>", "l"},
-            edit_vsplit = "s",
-            edit_split = "i"
-          }
-          vim.g.nvim_tree_icons = {
-            default = "",
-            symlink = "",
-            git = {
-              unstaged = "✚",
-              staged = "✚",
-              unmerged = "≠",
-              renamed = "≫",
-              untracked = "★"
-            }
-          }
-        end
-      }
-      -- view LSP symbol and tags
-      use {
-        "liuchengxu/vista.vim",
-        cmd = "Vista",
-        config = function()
-          vim.g["vista#renderer#enable_icon"] = 1
-          vim.g.vista_disable_statusline = 1
-          vim.g.vista_icon_indent = {"╰─▸ ", "├─▸ "}
-          vim.g.vista_default_executive = "ctags"
-          vim.g.vista_echo_cursor_strategy = "floating_win"
-          vim.g.vista_vimwiki_executive = "markdown"
-          vim.g.vista_executive_for = {
-            vimwiki = "markdown",
-            pandoc = "markdown",
-            markdown = "toc",
-            typescript = "nvim_lsp",
-            typescriptreact = "nvim_lsp"
-          }
-        end
-      }
-
-      -- Version control
-      use {
-        "mhinz/vim-signify",
-        event = {"BufReadPre *", "BufNewFile *"},
-        config = function()
-          vim.g.signify_sign_add = "▋"
-          vim.g.signify_sign_change = "▋"
-          vim.g.signify_sign_delete = "▋"
-          vim.g.signify_sign_delete_first_line = "▘"
-          vim.g.signify_sign_show_count = 0
-        end
-      }
-
-      -- LSP
-      use {
-        "neovim/nvim-lspconfig",
-        config = function()
-          require "conf.lsp"
-        end
-      }
-      use {
-        "alexaandru/nvim-lspupdate"
-      }
-      use "glepnir/lspsaga.nvim"
-
-      -- markdown language
-      if (vim.env.DISPLAY) then
-        use {
-          "iamcco/markdown-preview.nvim",
-          ft = "markdown",
-          run = "cd app && yarn install",
-          config = function()
-            vim.g.mkdp_auto_start = 0
-          end
+          },
+          ensure_installed = "all"
         }
       end
+    }
+    use {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      after = "nvim-treesitter"
+    }
 
-      -- Formatter
+    -- lua keymap
+    use "tjdevries/astronauta.nvim"
+
+    -- speed up jk
+    use "rhysd/accelerated-jk"
+
+    -- Indent Guides
+    use {
+      "glepnir/indent-guides.nvim",
+      config = function()
+        require("indent_guides").setup(
+          {
+            indent_levels = 30,
+            indent_guide_size = 1,
+            indent_start_level = 1,
+            indent_space_guides = true,
+            indent_tab_guides = false,
+            even_colors = {fg = "#ABB0BA", bg = "#34383F"},
+            odd_colors = {fg = "#BBC2CF", bg = "#2E323A"}
+          }
+        )
+      end
+    }
+
+    -- Commentary
+    use "tomtom/tcomment_vim"
+
+    -- Color highlighter
+    use {
+      "norcalli/nvim-colorizer.lua",
+      config = function()
+        require("colorizer").setup()
+      end
+    }
+
+    -- QuickRun
+    use {
+      "thinca/vim-quickrun",
+      setup = function()
+        vim.g.quickrun_no_default_key_mappings = 1
+      end,
+      config = function()
+        vim.api.nvim_set_keymap("n", "<leader>r", "<plug>QuickRun -mode n<cr>", {silent = true, noremap = true})
+        vim.api.nvim_set_keymap("v", "<leader>r", "<plug>QuickRun -mode v<cr>", {silent = true, noremap = true})
+      end
+    }
+
+    -- Fuzzy finder
+    -- use {"liuchengxu/vim-clap"}
+
+    use {
+      "nvim-telescope/telescope.nvim",
+      requires = {{"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"}},
+      config = function()
+        require("conf.telescope")
+      end
+    }
+
+    use {
+      "nvim-telescope/telescope-frecency.nvim",
+      requires = {
+        "tami5/sql.nvim"
+      },
+      config = function()
+        require "telescope".load_extension("frecency")
+        vim.api.nvim_set_keymap(
+          "n",
+          "<Leader>fm",
+          "<Cmd>lua require('telescope').extensions.frecency.frecency()<CR>",
+          {noremap = true, silent = true}
+        )
+      end
+    }
+
+    -- File explorer
+    use {
+      "kyazdani42/nvim-tree.lua",
+      cmd = {"NvimTreeToggle", "NvimTreeOpen"},
+      requires = {"kyazdani42/nvim-web-devicons"},
+      config = function()
+        vim.g.nvim_tree_follow = 1
+        vim.g.nvim_tree_hide_dotfiles = 1
+        vim.g.nvim_tree_indent_markers = 1
+        vim.g.nvim_tree_bindings = {
+          edit = {"<CR>", "l"},
+          edit_vsplit = "s",
+          edit_split = "i"
+        }
+        vim.g.nvim_tree_icons = {
+          default = "",
+          symlink = "",
+          git = {
+            unstaged = "✚",
+            staged = "✚",
+            unmerged = "≠",
+            renamed = "≫",
+            untracked = "★"
+          }
+        }
+      end
+    }
+    -- view LSP symbol and tags
+    use {
+      "liuchengxu/vista.vim",
+      cmd = "Vista",
+      config = function()
+        vim.g["vista#renderer#enable_icon"] = 1
+        vim.g.vista_disable_statusline = 1
+        vim.g.vista_icon_indent = {"╰─▸ ", "├─▸ "}
+        vim.g.vista_default_executive = "ctags"
+        vim.g.vista_echo_cursor_strategy = "floating_win"
+        vim.g.vista_vimwiki_executive = "markdown"
+        vim.g.vista_executive_for = {
+          vimwiki = "markdown",
+          pandoc = "markdown",
+          markdown = "toc",
+          typescript = "nvim_lsp",
+          typescriptreact = "nvim_lsp"
+        }
+      end
+    }
+
+    -- Version control
+    use {
+      "mhinz/vim-signify",
+      event = {"BufReadPre *", "BufNewFile *"},
+      config = function()
+        vim.g.signify_sign_add = "▋"
+        vim.g.signify_sign_change = "▋"
+        vim.g.signify_sign_delete = "▋"
+        vim.g.signify_sign_delete_first_line = "▘"
+        vim.g.signify_sign_show_count = 0
+      end
+    }
+
+    -- LSP
+    use {
+      "neovim/nvim-lspconfig",
+      config = function()
+        require "conf.lsp"
+      end
+    }
+    use {
+      "alexaandru/nvim-lspupdate"
+    }
+    use "glepnir/lspsaga.nvim"
+
+    -- markdown language
+    if (vim.env.DISPLAY) then
       use {
-        "mhartington/formatter.nvim",
+        "iamcco/markdown-preview.nvim",
+        ft = "markdown",
+        run = "cd app && yarn install",
         config = function()
-          require("conf.formatter")
-        end
-      }
-
-      -- Complete
-      use {
-        "hrsh7th/nvim-compe",
-        event = "InsertEnter *",
-        config = function()
-          require("conf.complete")
-        end
-      }
-
-      use {
-        "hrsh7th/vim-vsnip",
-        event = "InsertCharPre *"
-      }
-
-      -- use { "tzachar/compe-tabnine", run = "./install.sh" }
-
-      -- Translator
-      use {
-        "voldikss/vim-translator",
-        config = function()
-          vim.g.translator_history_enable = true
-          vim.g.translator_default_engines = {"bing", "haici", "youdao"}
+          vim.g.mkdp_auto_start = 0
         end
       }
     end
-  )
-end
 
-if not packer_exists then
-  packer_start()
-  packer.sync()
-end
+    -- Formatter
+    use {
+      "mhartington/formatter.nvim",
+      config = function()
+        require("conf.formatter")
+      end
+    }
 
-return packer_start()
+    -- Complete
+    use {
+      "hrsh7th/nvim-compe",
+      event = "InsertEnter *",
+      config = function()
+        require("conf.complete")
+      end
+    }
+
+    use {
+      "hrsh7th/vim-vsnip",
+      event = "InsertCharPre *"
+    }
+
+    -- use { "tzachar/compe-tabnine", run = "./install.sh" }
+
+    -- Translator
+    use {
+      "voldikss/vim-translator",
+      config = function()
+        vim.g.translator_history_enable = true
+        vim.g.translator_default_engines = {"bing", "haici", "youdao"}
+      end
+    }
+  end
+)
