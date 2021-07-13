c59a4f20147e3f7ad0a164e884feb017966be00e
Chang Ye <yech1990@gmail.com>
2021-07-13 15:43:37 -0500

    quick update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -250,12 +250,6 @@ return packer.startup(
         vim.g.nvim_tree_follow = 1
         vim.g.nvim_tree_hide_dotfiles = 1
         vim.g.nvim_tree_indent_markers = 1
-
-        vim.g.nvim_tree_bindings = {
-          ["l"] = ":lua require'nvim-tree'.on_keypress('edit')<CR>",
-          ["s"] = ":lua require'nvim-tree'.on_keypress('vsplit')<CR>",
-          ["i"] = ":lua require'nvim-tree'.on_keypress('split')<CR>"
-        }
         vim.g.nvim_tree_icons = {
           default = "",
           symlink = "",
