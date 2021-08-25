041efaeeb27dbd6dd321b0c4c3240c0ead55433f
Chang Ye <yech1990@gmail.com>
2021-08-25 13:10:14 -0500

    quick update

--- lua/conf/dashboard.lua
+++ lua/conf/dashboard.lua
@@ -13,3 +13,26 @@ vim.g.dashboard_custom_header = {
   "        ░ ░            ░",
   ""
 }
+
+vim.g.dashboard_custom_section = {
+  last_session = {
+    description = {"  Last Session                            SPC s l"},
+    command = "SessionLoad"
+  },
+  find_history = {
+    description = {"  Recently Opened Files                   SPC f h"},
+    command = "DashboardFindHistory"
+  },
+  find_file = {
+    description = {"  Find File                               SPC f f"},
+    command = "Telescope find_files find_command=rg,--hidden,--files,--glob,!.git"
+  },
+  new_file = {
+    description = {"  File Browser                            SPC f b"},
+    command = "Telescope file_browser"
+  },
+  find_word = {
+    description = {"  Find Word                               SPC f w"},
+    command = "DashboardFindWord"
+  }
+}
