dc70dfd70bacdb50a12b872cb2e531bbc49b8340
Chang Ye <yech1990@gmail.com>
2023-09-09 18:30:46 -0500

    quick update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -298,28 +298,7 @@ require("lazy").setup(
         "nvim-lua/plenary.nvim"
       },
       config = function()
-        require("gitsigns").setup {
-          signs = {
-            add = {hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn"},
-            change = {hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn"},
-            delete = {hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn"},
-            topdelete = {hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn"},
-            changedelete = {
-              hl = "GitSignsChange",
-              text = "~",
-              numhl = "GitSignsChangeNr",
-              linehl = "GitSignsChangeLn"
-            }
-          },
-          numhl = false,
-          linehl = false,
-          current_line_blame = false,
-          sign_priority = 6,
-          update_debounce = 100,
-          status_formatter = nil,
-          watch_gitdir = {interval = 1000},
-          diff_opts = {interval = true}
-        }
+        require("gitsigns").setup {}
       end
     },
     -- LSP
