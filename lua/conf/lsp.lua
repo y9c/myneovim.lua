e32900c680c4a1967fce383d2924dd11cf2d6320
Ye Chang <yech1990@gmail.com>
2023-12-05 17:36:23 -0600

    quick update

--- lua/conf/lsp.lua
+++ lua/conf/lsp.lua
@@ -40,30 +40,55 @@ end
 --   end
 -- }
 
-lspconfig.lua_ls.setup {
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {"vim"}
+if vim.tbl_contains({"lua_ls"}, servers) then
+  lspconfig.lua_ls.setup {
+    settings = {
+      Lua = {
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {"vim"}
+        }
       }
     }
   }
-}
+end
 
-lspconfig.yamlls.setup {
-  settings = {
-    yaml = {
-      keyOrdering = false
+if vim.tbl_contains({"yamlls"}, servers) then
+  lspconfig.yamlls.setup {
+    settings = {
+      yaml = {
+        keyOrdering = false
+      }
     }
   }
-}
+end
 
-lspconfig.ruff_lsp.setup {
-  on_attach = function(client, _)
-    -- Disable hover in favor of Pyright
-    client.server_capabilities.hoverProvider = false
-  end
-}
+if vim.tbl_contains({"ruff_lsp"}, servers) then
+  lspconfig.ruff_lsp.setup {
+    on_attach = function(client, _)
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  }
+end
+
+if vim.tbl_contains({"pyright"}, servers) then
+  lspconfig.pyright.setup {
+    on_attach = function(client, bufnr)
+      vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.executeCommandProvider = false
+    end,
+    capabilities = {
+      textDocument = {
+        publishDiagnostics = {
+          tagSupport = {
+            valueSet = {2}
+          }
+        }
+      }
+    }
+  }
+end
 
 -- https://github.com/glepnir/lspsaga.nvim
